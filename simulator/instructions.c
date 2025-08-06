33ce7b413ffea37b6c72d28fb34b10bd25f2ac75
Markel Mencía <markel.mnc@gmail.com>
2025-08-06 12:51:45 +0200

    Implement or instruction

--- simulator/instructions.c
+++ simulator/instructions.c
@@ -4,3 +4,8 @@ void i_add(int s1, int s2, int rd, Processor *p) {
     p->reg[rd] = p->reg[s1] + p->reg[s2];
     p->pc++;
 }
+
+void i_or(int s1, int s2, int rd, Processor *p) {
+    p->reg[rd] = p->reg[s1] | p->reg[s2];
+    p->pc++;
+}
