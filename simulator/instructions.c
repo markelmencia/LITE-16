2a5b3568fb49634482af37044a698ae1bfc42c0b
Markel Mencía <markel.mnc@gmail.com>
2025-08-06 12:54:16 +0200

    Implement and instruction

--- simulator/instructions.c
+++ simulator/instructions.c
@@ -14,3 +14,8 @@ void i_xor(int s1, int s2, int rd, Processor *p) {
     p->reg[rd] = p->reg[s1] ^ p->reg[s2];
     p->pc++;
 }
+
+void i_and(int s1, int s2, int rd, Processor *p) {
+     p->reg[rd] = p->reg[s1] & p->reg[s2];
+    p->pc++;
+}
