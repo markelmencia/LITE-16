05161a205f73c18cd0b677d96e06cd768e0a2fa6
Markel Mencía <markel.mnc@gmail.com>
2025-08-06 22:26:32 +0200

    Implement sl instruction

--- simulator/instructions.c
+++ simulator/instructions.c
@@ -16,6 +16,11 @@ void i_xor(int s1, int s2, int rd, Processor *p) {
 }
 
 void i_and(int s1, int s2, int rd, Processor *p) {
-     p->reg[rd] = p->reg[s1] & p->reg[s2];
+    p->reg[rd] = p->reg[s1] & p->reg[s2];
+    p->pc++;
+}
+
+void i_sl(int s1, int amm, int rd, Processor *p) {
+    p->reg[rd] = p->reg[s1] << amm;
     p->pc++;
 }
