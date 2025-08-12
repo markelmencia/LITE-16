2da2682af2017e3c159070bfcc5a2a557c0fece7
Markel Mencía <markel.mnc@gmail.com>
2025-08-12 13:41:32 +0200

    Create register fetch unit module

--- src/register-fetch-unit.v
+++ src/register-fetch-unit.v
@@ -1,5 +1,72 @@
 // Implementation of the LITE-16 Register Fetch Unit
 
+
+module register_fetch_unit(
+    input wire clk,
+    input wire [3:0] i4_7, // Instruction bits 4 to 7
+    input wire [3:0] i8_11, // Instruction bits 8 to 11
+    input wire [3:0] i12_15, // Instruction bits 12 to 15
+    input wire [15:0] r,
+    input wire ri,
+    input wire st,
+    input wire jmp,
+    input wire fn,
+
+    output reg [15:0] a,
+    output reg [15:0] b
+    );
+
+    // Bound to en, is set if the current instruction should store
+    // a value in a register
+    reg is_en;
+    // Enable bus. Each bit is bound to a register. If said bit is set,
+    // the value of that register will be set to r.
+    reg [15:0] en;
+    // The output of the register file.
+    wire [(16*16)-1:0] rf_out;
+    
+    register_file rf (clk, r, en, rf_out);
+    
+
+    // Generates an 16-register array, which is essentially
+    // the register bank.
+    reg [15:0] rf_array [0:15];
+    genvar j;
+    generate
+    for (j = 0; j < 16; j = j + 1) begin : array
+        always @(*) rf_array[j] = rf_out[j*16 +: 16];
+    end
+    endgenerate
+
+    always @(*) begin
+        /* R */
+        en = 0;
+
+        if (st || (jmp && !fn)) begin
+            is_en = 0;
+        end else begin
+            is_en = 1;
+        end
+
+        if (fn) begin
+            en[i8_11] = is_en;
+        end else begin
+            en[i12_15] = is_en;
+        end
+
+        /* A AND B OUTPUTS */
+        if (ri) begin
+            a = {12'b0, i4_7};
+            b = {i8_11, 12'b0};
+        end else begin
+            a = rf_array[i4_7];
+            b = rf_array[i8_11];
+        end
+    end
+    
+endmodule
+
+
 // Implements a register file, consisting
 // of 16 registers
 module register_file #(
--- test/register-fetch-unit_tb.v
+++ test/register-fetch-unit_tb.v
@@ -1,6 +1,67 @@
 // Implementation of the LITE-16 Register Fetch Unit
 `timescale 1ns/1ps
 
+
+module register_fetch_unit_tb;
+
+    reg clk;
+    reg [3:0] i4_7; // Instruction bits 4 to 7
+    reg [3:0] i8_11; // Instruction bits 8 to 11
+    reg [3:0] i12_15; // Instruction bits 12 to 15
+    reg [15:0] r;
+    reg ri;
+    reg st;
+    reg jmp;
+    reg fn;
+
+    wire [15:0] a;
+    wire [15:0] b;
+
+    register_fetch_unit rfu(clk, i4_7, i8_11, i12_15, r, ri, st, jmp, fn, a, b);
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        $dumpfile("register_fetch_unit_tb.vcd");
+        $dumpvars(0, rfu);
+
+        fn = 0;
+        jmp = 0;
+        ri = 0;
+        st = 1;
+
+        i4_7 = 1;
+        i8_11 = 2;
+        i12_15 = 1;
+        r = 16'h2222;
+        #5;
+
+        // No register changes
+        st = 0;
+        jmp = 1;
+        fn = 0;
+        #10;
+
+        // reg[2] should change to 0x2222
+        jmp = 0;
+        fn = 1;
+        #10;
+
+        fn = 0;
+        #10;
+
+        // reg[1] should change to 0x1111
+        r = 16'h1111;
+        #10;
+
+        ri = 1;
+        #10;
+        $finish;
+    end
+    
+endmodule
+
 module register_file_tb;
 
     reg clk;
