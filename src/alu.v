ceac05734dee2f8f7ab29589a7063b252caf2923
Markel Mencía <markel.mnc@gmail.com>
2025-08-13 16:38:08 +0200

    Create ALU module

---
+++ src/alu.v
@@ -0,0 +1,51 @@
+// Implementation of the LITE-16 Arithmetic Logic Unit
+
+module alu(
+    input wire [2:0] codeop,
+    input wire [15:0] a,
+    input wire [15:0] b,
+    input wire [15:0] rd,
+    input wire ri,
+    
+    output reg [15:0] r,
+    output reg cmp
+    );
+
+    reg [15:0] r0;
+    reg [15:0] r1;
+
+    always @(*) begin
+        /* R */
+        case (codeop)
+            3'b000: r0 = a + b;
+            3'b001: r0 = a | b;
+            3'b010: r0 = a ^ b;
+            3'b011: r0 = a & b;
+            3'b100: r0 = a << b;
+            3'b101: r0 = a >> b;
+            3'b110: r0 = a >>> b;
+            3'b111: r0 = a + b; // will concatenate
+        endcase
+
+        if (codeop[0]) begin
+            r1 = (a + b) + rd; // mv
+        end else begin
+            r1 = (a + b) << 8; // mvu
+        end
+
+        if (ri) begin
+            r = r1;
+        end else begin
+            r = r0;
+        end
+
+        /* CMP */
+        case(codeop[1:0])
+            2'b00: cmp = a == b;
+            2'b01: cmp = a < b;
+            2'b10: cmp = a > b;
+            2'b11: cmp = 1; // inconditional
+        endcase    
+    end
+
+endmodule
---
+++ test/alu_tb.v
@@ -0,0 +1,60 @@
+// Implementation of the LITE-16 Arithmetic Logic Unit
+
+module alu_tb;
+
+    reg [2:0] codeop;
+    reg [15:0] a;
+    reg [15:0] b;
+    reg [15:0] rd;
+    reg ri;
+    wire [15:0] r;
+    wire cmp;
+
+    alu alu_tb(codeop, a, b, rd, ri, r, cmp);
+
+    initial begin
+        $dumpfile("alu_tb.vcd");
+        $dumpvars(0, alu_tb);
+
+        a = 16'h0007;
+        b = 16'h0006;
+        rd = 16'h0005;
+
+        ri = 0;
+        codeop = 3'b000;
+        #10;
+        codeop = 3'b001;
+        #10;
+        codeop = 3'b010;
+        #10;
+        codeop = 3'b011;
+        #10;
+        codeop = 3'b100;
+        #10;
+        codeop = 3'b101;
+        #10;
+        codeop = 3'b110;
+        #10;
+        codeop = 3'b111;
+        #10;
+
+        ri = 1;
+        codeop = 3'b000;
+        #10;
+        codeop = 3'b001;
+        #10;
+        codeop = 3'b010;
+        #10;
+        codeop = 3'b011;
+        #10;
+        codeop = 3'b100;
+        #10;
+        codeop = 3'b101;
+        #10;
+        codeop = 3'b110;
+        #10;
+        codeop = 3'b111;
+        #10;
+    end
+
+endmodule
