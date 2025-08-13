0029ec04298a977587fc0a4e57ebeea158e04bd4
Markel Mencía <markel.mnc@gmail.com>
2025-08-14 00:27:18 +0200

    Remove REGISTER_COUNT parameter

--- src/register-fetch-unit.v
+++ src/register-fetch-unit.v
@@ -74,20 +74,18 @@ endmodule
 
 // Implements a register file, consisting
 // of 16 registers
-module register_file #(
-    parameter REGISTER_COUNT = 16
-)(
+module register_file(
     input wire clk,
     input wire rst,
     input wire [15:0] data_in,
-    input wire [REGISTER_COUNT-1:0] en,
+    input wire [15:0] en,
     output wire [(16*16)-1:0] data_out
-);
+    );
 
     // Generates 16 registers
     genvar i;
     generate
-        for (i = 0; i < REGISTER_COUNT; i = i + 1) begin : reg_array
+        for (i = 0; i < 16; i = i + 1) begin : reg_array
             register reg_u (
                 .clk(clk),
                 .rst(rst),
